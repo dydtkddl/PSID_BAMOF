452f7e6a476d453af7f295de3162ea9a8194f547
dydtkddl <dydtkddhkdwk@naver.com>
2025-06-23 13:19:02 +0900

    123

---
+++ 01_mof_ion_packmol/BAMOF_with_dissociated/run_all.py
@@ -0,0 +1,9 @@
+
+
+import os 
+files =[ "packmol_outputs/" + x for  x in os.listdir("packmol_outputs") ] 
+
+for i in files:
+    os.system(f"python packmol_filter_ver3.py  BAMOF_cp2k_opt-1_102_with_TFS.cif {i} Isolated_BMI-pos-final.xyz f") 
+
+
